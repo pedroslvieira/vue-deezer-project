bbd8a2078a5cf6d96c2f2ca89b04cacfa14a84b0
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-07-06 11:27:20 +0200

    built server from tutorial

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import process from "process";
+import path from "path";
+const port = process.env.PORT || 8080;
+
+const __dirname = path.dirname;
+
+const app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function (req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+app.listen(port);
+
+console.log("Server started...");
