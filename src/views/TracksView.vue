3cc2a6dc3096c64c1a2c007f56bfc03b7c76beaa
pedroslvieira <pedro.soares@mediaonegroup.ch>
2022-07-26 10:43:01 +0200

    metadata for lock screen

--- src/views/TracksView.vue
+++ src/views/TracksView.vue
@@ -12,6 +12,20 @@ const selectedTrack = ref({});
 // const response = await axios.get(apiUrl);
 // tracksApi.value = response.data.data;
 
+navigator.mediaSession.metadata = new MediaMetadata ({
+  title: 'Never Gonna Give You Up',
+  artist: 'Rick Astley',
+  album: 'Whenever You Need Somebody',
+  artwork: [
+    { src: 'https://dummyimage.com/96x96',   sizes: '96x96',   type: 'image/png' },
+    { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
+    { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
+    { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
+    { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
+    { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
+  ]
+});
+
 const componentKey = ref(0);
 const reloadComponent = () => {
   componentKey.value += 1;
