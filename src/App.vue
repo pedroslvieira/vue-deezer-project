d47d15700f1ab18be85b5cfd60b43bd6f378f712
Pedro Soares <pedro.soares@mediaonegroup.ch>
2022-10-11 11:17:10 +0200

    adding service worker to install

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup>
 import { RouterView } from "vue-router";
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the mini-infobar from appearing on mobile.
+  event.preventDefault();
+  console.log('👍', 'beforeinstallprompt', event);
+  // Stash the event so it can be triggered later.
+  window.deferredPrompt = event;
+  // Remove the 'hidden' class from the install button container.
+  divInstall.classList.toggle('hidden', false);
+});
 </script>
 
 <template>
